d64575f9a76eff5a10c7014bb62875c7fa04591a
zyfayes <zyfayes@users.noreply.github.com>
2026-03-24 11:46:27 +0000

    deploy: f742ed8edffba554e12e63ba7dfb4cf303121984

---
+++ assets/AlvaChat-BaEoew3F.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-B_qFdrtA.js";function f({onNavigate:r}){return t.useEffect(()=>{r("home-v3")},[r]),null}export{f as default};
--- assets/Skills-FahpffR7.js
+++ assets/Skills-FahpffR7.js
@@ -1,4 +1,4 @@
-import{r as k,j as e}from"./index-BtpkKV-6.js";import{a as Y,U as $}from"./AppShell-CozoMBJJ.js";const J=`
+import{r as k,j as e}from"./index-B_qFdrtA.js";import{a as Y,U as $}from"./AppShell-BhR8s1fM.js";const J=`
 .skill-modal-scroll ::-webkit-scrollbar { width: 4px; height: 4px; }
 .skill-modal-scroll ::-webkit-scrollbar-track { background: transparent; }
 .skill-modal-scroll ::-webkit-scrollbar-thumb { background: transparent; border-radius: 2px; }
