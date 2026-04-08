de5dfadb950bd4bc0aef00b8b2270f9336ee1c70
sheer-creator <sheer-creator@users.noreply.github.com>
2026-04-08 14:15:15 +0000

    deploy: 8b7a5ce6adf9b89cfc0df615055e2cf2425a904c

--- assets/AlvaChat-DNzjFSo8.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-DWzjcJyM.js";function f({onNavigate:r}){return t.useEffect(()=>{r("home-v3")},[r]),null}export{f as default};
---
+++ assets/AlvaChat-quAVX6UZ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-Dk0wULUc.js";function f({onNavigate:r}){return t.useEffect(()=>{r("home")},[r]),null}export{f as default};
